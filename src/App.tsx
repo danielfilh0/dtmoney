7af060f0c8702bf557772017d93247b6886c7825
Daniel Filho <danbsilva98@gmail.com>
2022-03-31 14:32:31 -0300

    feat: add transactions table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { TransactionsTable } from "./components/TransactionsTable";
 
 export function App() {
   return (
@@ -8,6 +9,7 @@ export function App() {
       <GlobalStyle />
       <Header />
       <Dashboard />
+      <TransactionsTable />
     </>
   );
 }
---
+++ src/components/TransactionsTable/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    margin-top: 4rem;
+    max-width: 1120px;
+    margin: 0 auto;
+
+    table {
+        width: 100%;
+        border-spacing: 0 0.5rem;
+
+        th {
+            font-weight: 400;
+            color: var(--text-body);
+            padding: 1rem 2rem;
+            text-align: left;
+            line-height: 1.5rem;
+        }
+
+        td {
+            padding: 1rem 2rem;
+            border: 0;
+            background: var(--shape);
+            color: var(--text-body);
+            border-radius: 0.25rem;
+
+            &:first-of-type {
+                color: var(--text-title);
+            }
+
+            &.deposit {
+                color: var(--green);
+            }
+
+            &.withdraw {
+                color: var(--red);
+            }
+        }
+    }
+`;
